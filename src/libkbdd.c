29ea6665569ebc0ed1229eba944fd5c21d84ffd9
Alexander Vershilov <alexander.vershilov@gmail.com>
2011-04-09 07:55:01 +0000

    Fixing error in previous commit

--- src/libkbdd.c
+++ src/libkbdd.c
@@ -317,6 +317,7 @@ _on_xkbEvent(XkbEvent ev)
             uint32_t grp = ev.state.group;
             Window focused_win;
             int revert;
+            XGetInputFocus( ev.any.display, &focused_win, &revert);      
             if (grp == ev.state.locked_group) //do not save layout with modifier
                 _kbdd_update_window_layout( focused_win, grp);
             break;
